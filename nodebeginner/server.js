98b919232c0371e82567c0225732a055d0ad4750
Jongho Lim <sloth@kookmin.ac.kr>
2015-12-02 23:22:14 +0900

    update server.js   checking work server.

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -4,6 +4,10 @@ function onRequest( request, response ) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write( "Hello World" );
   response.end();
+
+  console.log("send hello world");
 }
 
 http.createServer( onRequest ).listen(19917);
+
+console.log("Server running on 19917");
