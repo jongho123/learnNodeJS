2ef5459264c22fb3a39f69de28931703ee09eba3
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-10 20:24:42 +0900

    update asyncfs.js   - using async module

--- callback/asyncfs.js
+++ callback/asyncfs.js
@@ -1,17 +1,29 @@
-var fs = require('fs');
+var fs = require('fs'),
+    async = require('async');
 
 try {
-  // 파일 읽음.
-  fs.readFile('./apples.txt', 'utf8', function(err, data) {
-    if(err) throw err;
-
-    // 내용 수정 'apple', 'Apple' -> 'orange'
-    var adjData = data.replace(/[A|a]pple/g, 'orange');
-    
-    // 변경된 내용을 oranges.txt 에 씀
-    fs.writeFile('./oranges.txt', adjData, function(err) {
-      if(err) throw err;
-    });
+  async.waterfall([
+    // 파일 읽음.
+    function readData(callback) {
+      fs.readFile('./data/data1.txt', 'utf8', function(err, data) {
+        callback(err, data);
+      });
+    },
+    // 내용 수정 'somecompany.com' -> 'burningbird.net'
+    function modify(text, callback) {
+      var adjData = text.replace(/somecompany\.com/g, 'burningbird.net');
+      callback(null, adjData);
+    },
+    // 변경된 내용을 덮어 씀
+    function writeData(text, callback) {
+      fs.writeFile('./data/data1.txt', text, function(err) {
+        callback(err, text);
+      });
+    }
+    // 결과 출력
+  ], function(err, result) {
+       if(err) throw err;
+       console.log(result);
   });
 } catch(err) {
   console.error(err);
