8b728d8308137766a3103b1ab49f22c869fa807f
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-10 20:04:37 +0900

    update asyncDirfs.js   - check isFile   - but callback Spaghetti(or Pyramid of Doom)

--- callback/asyncDirfs.js
+++ callback/asyncDirfs.js
@@ -10,16 +10,21 @@ try {
   fs.readdir('./data/', function(err, files) {
     // 각 파일에 대해
     files.forEach(function(name) {
-      fs.readFile('./data/' + name, 'utf8', function(err, data) {
+      fs.stat('./data/' + name, function(err, stats) {
         if(err) throw err;
-        var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
-
-        fs.writeFile('./data/' + name, adjData, function(err) {
-          if(err) throw err;
-          writeStream.write('change ' + name + '\n', 'utf8', function(err) {
+ 
+        if(stats.isFile())
+          fs.readFile('./data/' + name, 'utf8', function(err, data) {
             if(err) throw err;
+            var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
+
+            fs.writeFile('./data/' + name, adjData, function(err) {
+              if(err) throw err;
+              writeStream.write('change ' + name + '\n', 'utf8', function(err) {
+                if(err) throw err;
+              });
+            });
           });
-        });
       });
     }); 
   });
