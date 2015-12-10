dd5a53c50f1d1e76da69692c3dabb4035771ea3e
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-10 19:56:17 +0900

    add asyncDirfs.js   - replace files in directory async...

---
+++ callback/asyncDirfs.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+var writeStream = fs.createWriteStream('./log.txt',
+  {'flags': 'a',
+   'encoding': 'utf8',
+   'mode': 0666});
+
+try {
+  // 파일의 목록을 가져옴
+  fs.readdir('./data/', function(err, files) {
+    // 각 파일에 대해
+    files.forEach(function(name) {
+      fs.readFile('./data/' + name, 'utf8', function(err, data) {
+        if(err) throw err;
+        var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
+
+        fs.writeFile('./data/' + name, adjData, function(err) {
+          if(err) throw err;
+          writeStream.write('change ' + name + '\n', 'utf8', function(err) {
+            if(err) throw err;
+          });
+        });
+      });
+    }); 
+  });
+} catch(err) {
+  console.error(err);
+}
