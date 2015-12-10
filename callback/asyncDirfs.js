219c85a836aa564cc2e6d4b5e9b190a4ea046e8a
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-10 20:45:28 +0900

    update asyncDirfs.js   - using waterfall in async module

--- callback/asyncDirfs.js
+++ callback/asyncDirfs.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
+var fs = require('fs'),
+    async = require('async'),
+    _dir = './data/';
 
 var writeStream = fs.createWriteStream('./log.txt',
   {'flags': 'a',
@@ -6,27 +8,53 @@ var writeStream = fs.createWriteStream('./log.txt',
    'mode': 0666});
 
 try {
-  // 파일의 목록을 가져옴
-  fs.readdir('./data/', function(err, files) {
-    // 각 파일에 대해
-    files.forEach(function(name) {
-      fs.stat('./data/' + name, function(err, stats) {
-        if(err) throw err;
- 
-        if(stats.isFile())
-          fs.readFile('./data/' + name, 'utf8', function(err, data) {
-            if(err) throw err;
-            var adjData = data.replace(/somecompany\.com/g, 'burningbird.net');
-
-            fs.writeFile('./data/' + name, adjData, function(err) {
-              if(err) throw err;
-              writeStream.write('change ' + name + '\n', 'utf8', function(err) {
-                if(err) throw err;
-              });
-            });
-          });
+  async.waterfall([
+    // 파일의 목록을 가져옴
+    function readDir(callback) {
+      fs.readdir(_dir, function(err, files) {
+        callback(err, files);
       });
-    }); 
+    },
+    // 각 파일에 대해
+    function loopFiles(files, callback) {
+      files.forEach(function(name) {
+        callback(null, name);
+      });
+    },
+    // file인지 체크 함.
+    function checkFile(file, callback) {
+      fs.stat(_dir + file, function(err, stats) {
+        callback(err, stats, file);
+      });
+    },
+    // 파일을 읽음.
+    function readData(stats, file, callback) {
+      if(stats.isFile())
+        fs.readFile(_dir + file, 'utf8', function(err, data) {
+          callback(err, file, data);
+        });
+    },
+    // 내용을 수정.
+    function modify(file, text, callback) {
+      var adjData = text.replace(/somecompany\.com/g, 'burningbird.net');
+      callback(null, file, adjData); 
+    },
+    // 수정된 내용을 저장
+    function writeData(file, text, callback) {
+      fs.writeFile(_dir + file, text, function(err) {  
+        callback(err, file);
+      });
+    },
+    // 수정된 파일 이름을 로그로 남김
+    function logChange(file, callback) {
+      writeStream.write('change ' + file + '\n', 'utf8', function(err) {
+        callback(err, file);
+      });
+    }
+    // 결과를 출력
+  ], function(err, result) {
+       if(err) throw err; 
+       console.log(result);
   });
 } catch(err) {
   console.error(err);
