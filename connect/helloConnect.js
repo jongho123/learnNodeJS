8ddde65e81e2a1f20499b27506330b430c8381f0
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-11 20:43:43 +0900

    update helloConnect.js   - add middleware serve-static   - mod morgan format combined -> dev, using stream

--- connect/helloConnect.js
+++ connect/helloConnect.js
@@ -1,13 +1,19 @@
 var connect = require('connect'),
     morgan = require('morgan'),
-    favicon = require('serve-favicon');
-var http = require('http');
+    favicon = require('serve-favicon'),
+    serveStatic = require('serve-static');
+
+var http = require('http'),
+    fs = require('fs');
+
+var writeStream = fs.createWriteStream('./log.txt',
+      {'flags': 'a',
+       'encoding': 'utf8',
+       'mode':0666});
 
 var app = connect()
          .use(favicon(__dirname + '/favicon.ico'))
-         .use(morgan('combined'))
-         .use(function(req, res) {
-           res.end('Hello world\n');
-         });
+         .use(morgan('dev', {stream: writeStream}))
+         .use(serveStatic(__dirname + '/static', {index: 'public.html'}));
 
 http.createServer(app).listen(19918);
