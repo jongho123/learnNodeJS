1538bcaea76ad1091a8d6dda393c21734b676a08
JongHo Lim <sloth@kookmin.ac.kr>
2015-12-11 22:26:58 +0900

    add urlRoute.js   - using crossroads module   - route url

---
+++ crossroads/urlRoute2.js
@@ -0,0 +1,15 @@
+var crossroads = require('crossroads'),
+    http = require('http');
+
+var typeRoute = crossroads.addRoute('/{type}/{id}');
+
+function onTypeAccess(type, id) {
+  console.log('access ' + type + ' ' + id);
+}
+
+typeRoute.matched.add(onTypeAccess);
+
+http.createServer(function(req, res) {
+  crossroads.parse(req.url);
+  res.end('processing');
+}).listen(19918);
